6da52ae2c68d7001545a4bede317e97761dfb483
Your Name <you@example.com>
2020-12-12 00:46:31 +0800

    add junit

--- Demo01/src/test/java/com/TestIndex.java
+++ Demo01/src/test/java/com/TestIndex.java
@@ -1,10 +1,15 @@
 package com;
 
+import com.yanchen.Check;
 import org.junit.Test;
 
 public class TestIndex {
     @Test
     public void show(){
         System.out.println("show");
+    }
+    @Check(name = "zhangsan")
+    public void calculate(){
+        System.out.printf("cal");
     }
 }
