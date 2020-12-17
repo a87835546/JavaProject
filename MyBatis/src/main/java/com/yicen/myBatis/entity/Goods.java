8314fc702030296b53a2926f9cd39b8431862cd3
Your Name <you@example.com>
2020-12-17 16:25:11 +0800

    add mybatis使用

---
+++ MyBatis/src/main/java/com/yicen/myBatis/entity/Goods.java
@@ -0,0 +1,76 @@
+package com.yicen.myBatis.entity;
+
+public class Goods {
+    private String goodsId;
+    private String title;
+    private String subTitle;
+    private Float originalCost;
+    private Float currentPrice;
+    private Float discount;
+    private Integer isFreeDelivery;
+    private String categoryId;
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public Float getOriginalCost() {
+        return originalCost;
+    }
+
+    public void setOriginalCost(Float originalCost) {
+        this.originalCost = originalCost;
+    }
+
+    public Float getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(Float currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public Float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Float discount) {
+        this.discount = discount;
+    }
+
+    public Integer getFreeDelivery() {
+        return isFreeDelivery;
+    }
+
+    public void setFreeDelivery(Integer freeDelivery) {
+        isFreeDelivery = freeDelivery;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+}
--- MyBatis/src/test/java/TestMybatis.java
+++ MyBatis/src/test/java/TestMybatis.java
@@ -1,12 +1,40 @@
+import com.yicen.myBatis.entity.Goods;
 import com.yicen.myBatisUtils.MyBatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class TestMybatis {
 
     @Test
     public void test(){
-        SqlSession session = MyBatisUtils.getSqlSession();
-        session.insert("");
+        SqlSession session = null;
+        try {
+          session =  MyBatisUtils.getSqlSession();
+            System.out.println(session);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+
+    @Test
+    public void testSelectAll() throws Exception{
+        SqlSession session = null;
+        try {
+            session = MyBatisUtils.getSqlSession();
+            List<Goods> list = session.selectList("goods.selectAll");
+            for (Goods d: list){
+                System.out.println(d.getTitle());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }finally {
+            MyBatisUtils.closeSession(session);
+        }
     }
 }
