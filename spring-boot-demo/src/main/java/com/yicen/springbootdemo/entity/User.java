497fa5f04b445b1d896349c36159e40807fb2605
Your Name <you@example.com>
2021-01-05 03:30:24 +0800

    add spring boot

---
+++ spring-boot-demo/src/main/java/com/yicen/springbootdemo/entity/User.java
@@ -0,0 +1,10 @@
+package com.yicen.springbootdemo.entity;
+
+import lombok.Data;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Data
+public class User {
+    private String id;
+}
---
+++ spring-boot-demo/src/main/java/com/yicen/springbootdemo/service/UserService.java
@@ -0,0 +1,9 @@
+package com.yicen.springbootdemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
